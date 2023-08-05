00a4af4a3a99767fdcedf7012efae6cb9bdb40f6
Kiran <krnsundal@gmail.com>
2023-08-05 12:39:52 +0100

    Passing props wiithout a type covered

--- ts-with-react/src/components/PostCard/PostCard.tsx
+++ ts-with-react/src/components/PostCard/PostCard.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 /*
 React-TypeScript: Passing props
--- ts-with-react/src/components/PostList/PostList.tsx
+++ ts-with-react/src/components/PostList/PostList.tsx
@@ -2,14 +2,23 @@ import React from "react";
 import PostCard from "../PostCard/PostCard";
 
 const PostList = async () => {
+  async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
 
+    return res.json();
+  }
+  const data = await getData();
 
   return (
     <div>
-      <PostCard title="Post Title" body="Post Desc" />
-
-
+      {/* <PostCard title="Post Title" body="Post Desc" /> */}
+      {data.map((post: { id: number; title: string; body: string }) => {
+        <PostCard key={post.id} title={post.title} body={post.body} />;
+      })}
     </div>
   );
 };
