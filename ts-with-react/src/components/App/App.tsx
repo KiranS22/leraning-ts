dd61b983e3c419bac46927022c901f3a8e517e23
Kiran <krnsundal@gmail.com>
2023-08-05 11:50:49 +0100

    Refactored ts-react app structure to my liking
    added components and resources folders to hold comps and comp css

---
+++ ts-with-react/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../../resources/App/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My React-TypeScript app is running </h1>
+    </div>
+  );
+}
+
+export default App;
